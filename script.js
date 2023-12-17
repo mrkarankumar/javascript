167e8e4a869251431752b6d297a2ba7dcda25fc6
karan kumar <karanips0009@gmail.com>
2023-12-17 18:08:06 +0530

    java script commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let fullname ="karan kumar chaudhary";
+
+console.log("my name is karan kumar chaudhary ");
+console.log("hello my best friend raja ");
+console.log("hello apna coading  kya hal ha bro ");
+
+
+console.log(fullname)
