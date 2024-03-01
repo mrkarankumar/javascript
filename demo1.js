02849efd425975f570c39ab2cf5253ab52f3c680
karan kumar <karanips0009@gmail.com>
2024-03-01 15:06:39 +0530

    commit

--- demo1.js
+++ demo1.js
@@ -1,3 +1,14 @@
 function myFunction() {
     document.getElementById("karan").innerHTML = "my name is rocky rohit kumar chaudhary";
   }
+
+
+
+  console.log("karan mumar ")
+  console.log("karan mumar ")
+  console.log("karan mumar ")
+  console.log("karan mumar ")
+  console.log("karan mumar ")
+  console.log("karan mumar ")
+  console.log("karan mumar ")
+  
